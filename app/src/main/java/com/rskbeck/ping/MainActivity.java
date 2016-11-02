78ee0e3c738637864b9d3f597d4b034511e004a6
Ryan Kehlenbeck <rskbeck@gmail.com>
2016-11-02 09:53:07 -0700

    Added needed keys for firebase and map

--- app/src/main/java/com/rskbeck/ping/MainActivity.java
+++ app/src/main/java/com/rskbeck/ping/MainActivity.java
@@ -4,12 +4,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Configure Google Sign In
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.my_web_client_id))
+                .requestEmail()
+                .build();
 
         Log.d("LOG", "New Commit");
     }
