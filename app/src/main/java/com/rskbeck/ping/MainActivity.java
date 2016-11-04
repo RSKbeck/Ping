add22235efc80c37938927dec02addf8c05a60ae
xianguang13 <xianguang_yan@hotmail.com>
2016-11-04 07:57:29 -0700

    Test

--- app/src/main/java/com/rskbeck/ping/MainActivity.java
+++ app/src/main/java/com/rskbeck/ping/MainActivity.java
@@ -1,17 +1,26 @@
 package com.rskbeck.ping;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
 
+import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 
 public class MainActivity extends AppCompatActivity {
-
+    private GoogleApiClient mGoogleApiClient;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.sign_in_button).setOnClickListener((View.OnClickListener) this);
+
+        //this.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         // Configure Google Sign In
         GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
@@ -19,6 +28,17 @@ public class MainActivity extends AppCompatActivity {
                 .requestEmail()
                 .build();
 
-        Log.d("LOG", "New Commit");
+        // Build a GoogleApiClient with access to the Google Sign-In API and the
+        // options specified by gso.
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, new GoogleApiClient.OnConnectionFailedListener() {
+                    @Override
+                    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+                        Log.d("LOGINLOG", "CONNECTION FAILED");
+                    }
+                })
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
     }
 }
